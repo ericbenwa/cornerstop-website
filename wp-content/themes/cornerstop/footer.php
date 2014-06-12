9ea8859e393de03d220d845c2287e776cd97ed86
Eric Benoit <eric@goinvo.com>
2014-06-11 23:59:28 -0400

    Custom theme chrome

---
+++ wp-content/themes/cornerstop/footer.php
@@ -0,0 +1,36 @@
+			<footer role="contentinfo">
+			
+				<div id="inner-footer" class="clearfix">
+		          <hr />
+		          <div id="widget-footer" class="clearfix row">
+		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
+		            <?php endif; ?>
+		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2') ) : ?>
+		            <?php endif; ?>
+		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
+		            <?php endif; ?>
+		          </div>
+					
+					<nav class="clearfix">
+						<?php wp_bootstrap_footer_links(); // Adjust using Menus in Wordpress Admin ?>
+					</nav>
+			
+					<p class="attribution"><strong>Lunch</strong>: Wednesday – Sunday 11:30-2:30 • <strong>Bar menu &amp; drinks</strong> in between • <strong>Dinner</strong> nightly at 5pm<br />
+					781.875.3065 • 235 Hull Street, Cohasset MA 02025</p>
+				
+				</div> <!-- end #inner-footer -->
+				
+			</footer> <!-- end footer -->
+		
+		</div> <!-- end #container -->
+				
+		<!--[if lt IE 7 ]>
+  			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+  			<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+		<![endif]-->
+		
+		<?php wp_footer(); // js scripts are inserted using this function ?>
+
+	</body>
+
+</html>
