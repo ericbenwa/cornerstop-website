625627f04352e23e1de9b29c9116b6007042ccdc
Eric Benoit <eric@goinvo.com>
2014-06-13 00:27:56 -0400

    Column width adjustments

--- wp-content/themes/cornerstop/sidebar.php
+++ wp-content/themes/cornerstop/sidebar.php
@@ -1,8 +1,19 @@
-				<div id="sidebar1" class="col-sm-3" role="complementary">
+				<div id="sidebar1" class="col-sm-2 col-sm-offset-1" role="complementary">
 				
 					<!-- <?php if ( is_active_sidebar( 'sidebar1' ) ) : ?> -->
 
 						<!-- <?php dynamic_sidebar( 'sidebar1' ); ?> -->
+
+						<a href="/"><img src="http://cornerstop.local/wp-content/uploads/2014/06/corner-stop-eatery-restaurant.png" /></a>
+						<h2>Eat</h2>
+						<h3><a href="#dinner">Dinner</a></h3>
+						<h3><a href="#lunch">Lunch</a></h3>
+						<h3><a href="#kids">Kid's</a></h3>
+
+						<h2>Drink</h2>
+						<h3><a href="#wines">Wines</a></h3>
+						<h3><a href="#cocktails">Cocktails</a></h3>
+						<h3><a href="#beers">Beers</a></h3>
 
 					<?php else : ?>
 
