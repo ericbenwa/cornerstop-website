64597cb5f5a956dc8e4698bf9f26de764ed4f25d
Eric Benoit <eric@goinvo.com>
2014-06-16 22:56:29 -0400

    Updated Title tag

--- wp-content/themes/cornerstop/header.php
+++ wp-content/themes/cornerstop/header.php
@@ -9,7 +9,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title><?php wp_title( '|', true, 'right' ); ?></title>	
+		<title><?php wp_title( '|', true, 'right' ); ?> The Corner Stop Eatery</title>	
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				
 		<!-- media-queries.js (fallback) -->
