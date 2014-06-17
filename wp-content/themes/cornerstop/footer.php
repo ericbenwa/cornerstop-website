10ffb88fed531690b2631f2ff1c8d0cb35871991
Eric Benoit <eric@goinvo.com>
2014-06-16 22:56:48 -0400

    Footer visible for index page only

--- wp-content/themes/cornerstop/footer.php
+++ wp-content/themes/cornerstop/footer.php
@@ -14,9 +14,11 @@
 					<nav class="clearfix">
 						<?php wp_bootstrap_footer_links(); // Adjust using Menus in Wordpress Admin ?>
 					</nav>
-			
-					<!-- <p class="attribution"><strong>Lunch</strong>: Wednesday – Sunday 11:30-2:30 • <strong>Bar menu &amp; drinks</strong> in between • <strong>Dinner</strong> nightly at 5pm<br />
-					<a href="tel:781-875-3065">781.875.3065</a> • <a href="https://www.google.com/maps/place/The+Corner+Stop+Eatery/@42.259371,-70.845015,17z/data=!3m1!4b1!4m2!3m1!1s0x0:0x604bb783d4e9e727">235 Hull Street, Cohasset MA 02025</a></p> -->
+
+					<?php if ( is_page('index')) { ?>
+						<p class="attribution"><strong>Lunch</strong>: Wednesday – Sunday 11:30-2:30 • <strong>Bar menu &amp; drinks</strong> in between • <strong>Dinner</strong> nightly at 5pm<br />
+						<a href="tel:781-875-3065">781.875.3065</a> • <a href="https://www.google.com/maps/place/The+Corner+Stop+Eatery/@42.259371,-70.845015,17z/data=!3m1!4b1!4m2!3m1!1s0x0:0x604bb783d4e9e727">235 Hull Street, Cohasset MA 02025</a></p>
+					<?php }	 ?>
 				
 				</div> <!-- end #inner-footer -->
 				
