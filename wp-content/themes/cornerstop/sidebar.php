000adbc31978c2f5ed030f5039c24772bd63618c
Eric Benoit <eric@goinvo.com>
2014-06-29 16:33:55 -0400

    New logo, favicon, style tweaks for menu+blog+misc

--- wp-content/themes/cornerstop/sidebar.php
+++ wp-content/themes/cornerstop/sidebar.php
@@ -4,7 +4,7 @@
 
 						<!-- <?php dynamic_sidebar( 'sidebar1' ); ?> -->
 
-						<a href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/corner-stop-eatery-restaurant.png" /></a>
+						<a href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/corner-stop-eatery-restaurant-slim.png" /></a>
 
 						<?php if ( is_page('visit')) { ?>
 
