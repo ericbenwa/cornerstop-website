df9c6fcda3584d007d990af600c352fe00100d0f
Eric Benoit <eric@goinvo.com>
2014-08-04 21:43:46 -0400

    Reverted back to dynamically built nag

--- wp-content/themes/cornerstop/navbar.php
+++ wp-content/themes/cornerstop/navbar.php
@@ -12,14 +12,7 @@
 					</div>
 
 					<div class="collapse navbar-collapse navbar-responsive-collapse">
-						<ul id="menu-main-menu" class="nav navbar-nav">
-							<li id="menu-item-17" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="<?php echo home_url(); ?>/eat-drink/">Eat + Drink</a></li>
-							<li id="menu-item-14" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="<?php echo home_url(); ?>/our-story/">Our Story</a></li>
-							<li id="menu-item-130" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="<?php echo home_url(); ?>/happenings/">Happenings</a></li>
-							<li id="menu-item-6" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="<?php echo home_url(); ?>/private-parties/">Private Parties</a></li>
-							<li id="menu-item-11" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="#ex1" rel="modal:open">Gift Certificates</a></li>
-							<li id="menu-item-8" class="menu-item menu-item-type-post_type menu-item-object-page"><a href="<?php echo home_url(); ?>/visit/">Visit</a></li>
-						</ul>
+						<?php wp_bootstrap_main_nav(); // Adjust using Menus in Wordpress Admin ?>
 					</div>
 
 				</div> <!-- end .navbar -->
